bf0b7d8c2e5bd5b7c7a34f8f26c3d5dfa06fe0c0
guruprasadsudhagar-star <guruprasadsudhagar@gmail.com>
2026-05-17 12:12:18 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
+from astar import astar
+from visualize import show_grid
 
+grid = [
+    [0, 0, 0, 0, 0],
+    [1, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0]
+]
+
+start = (0, 0)
+goal = (4, 4)
+
+path = astar(grid, start, goal)
+
+print("Path found:")
+print(path)
+
+show_grid(grid, start, goal, path)
